bac85f4e3f6d3cfa1344e9dd06f77acb3c5879ff
zachwilson21 <zchbob2@yahoo.com>
2022-04-10 14:58:40 -0400

    fixed spelling

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -10,10 +10,10 @@ class FAQ extends Component {
             <div id='FAQ-Section' className='scrollbar-FAQ' />
                 <div className='faq-header'>FAQ</div>
                 <div className='faq-section-title'> How Many Jars Can I Add To My Server?
-                    <div className='faq-section-answer'>Each server is permitted 1 jar at a time. The server owner may delete the jar at any time and craete a new one.</div>
+                    <div className='faq-section-answer'>Each server is permitted 1 jar at a time. The server owner may delete the jar at any time and create a new one.</div>
                 </div>
                 <div className='faq-section-title'> Who Can Create/Delete A Jar?
-                    <div className='faq-section-answer'>Only the server owner is permitted to create and delte jars in the server. </div>
+                    <div className='faq-section-answer'>Only the server owner is permitted to create and delete jars in the server. </div>
                 </div>
                 <div className='faq-section-title'> Who Can View My Messages?
                     <div className='faq-section-answer'>Each server user can view their own messages and nobody elses before the jar is opened. The only exception is that the server owner may view all messages in the jar at any time for review.</div>
